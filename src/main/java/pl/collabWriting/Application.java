5ed7ab69bf1b96b1ac8f6338a5e5f14875ff52e4
kattie95 <szikar95@gmail.com>
2017-02-27 15:00:38 +0100

    Initial commit

---
+++ src/main/java/pl/collabWriting/Application.java
@@ -0,0 +1,16 @@
+package pl.collabWriting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kattie95
+ */
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
