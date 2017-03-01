d05a99d78fcc785c19ae9eb10090dbed859482f5
kattie95 <szikar95@gmail.com>
2017-03-01 19:57:31 +0100

    Create Comment and Role

---
+++ src/main/java/pl/collabWriting/domain/Comment.java
@@ -0,0 +1,63 @@
+package pl.collabWriting.domain;
+
+import javax.persistence.*;
+
+/**
+ * @author kattie95
+ */
+
+@Entity
+@Table (name = "comments")
+public class Comment
+{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Post post;
+
+    private String commentBody;
+
+    private Comment()
+    {
+        //
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public String getCommentBody() {
+        return commentBody;
+    }
+
+    public void setCommentBody(String commentBody) {
+        this.commentBody = commentBody;
+    }
+}
---
+++ src/main/java/pl/collabWriting/domain/Role.java
@@ -0,0 +1,73 @@
+package pl.collabWriting.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * @author kattie95
+ */
+
+@Entity
+@Table (name = "roles")
+public class Role
+{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String role;
+    private boolean makeStory;
+    private boolean makePost;
+
+    @OneToMany
+    private Set<User> users;
+
+    private Role()
+    {
+        //
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isMakeStory() {
+        return makeStory;
+    }
+
+    public void setMakeStory(boolean makeStory) {
+        this.makeStory = makeStory;
+    }
+
+    public boolean isMakePost() {
+        return makePost;
+    }
+
+    public void setMakePost(boolean makePost) {
+        this.makePost = makePost;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
