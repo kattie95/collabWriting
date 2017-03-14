ae4537407ecbbfb260be0e8c093cab66acad20e0
kattie95 <szikar95@gmail.com>
2017-03-14 18:26:52 +0100

    Create html files for log in and sign up and update css

--- src/main/java/pl/collabWriting/controller/HomeConroller.java
+++ src/main/java/pl/collabWriting/controller/HomeConroller.java
@@ -1,7 +1,10 @@
 package pl.collabWriting.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.collabWriting.service.PostService;
 
 /**
  * @author kattie95
@@ -10,9 +13,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeConroller
 {
+    private PostService postService;
+
+    @Autowired
+    public HomeConroller(PostService postService) {
+        this.postService = postService;
+    }
+
     @RequestMapping("/")
-    public String home()
+    public String home(Model model)
     {
+        model.addAttribute("post", postService.getLastUpdatedPost());
         return "index";
     }
+
+    @RequestMapping("/sign")
+    public String aboutSign()
+    {
+        return "user/aboutSign";
+    }
+
+    @RequestMapping("/login")
+    public String login()
+    {
+        return "user/login";
+    }
+
 }
