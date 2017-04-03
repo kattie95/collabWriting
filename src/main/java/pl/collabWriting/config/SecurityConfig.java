87ecb263b08cb298cc7014ce65048240b8505ac4
kattie95 <szikar95@gmail.com>
2017-04-03 16:23:09 +0200

    Add character encoding

--- src/main/java/pl/collabWriting/config/SecurityConfig.java
+++ src/main/java/pl/collabWriting/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 /**
  * @author kattie95
@@ -47,5 +49,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .logout()
                     .logoutSuccessUrl("/login?logout")
                     .permitAll();
+
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        httpSecurity.addFilterBefore(filter,CsrfFilter.class);
     }
 }
